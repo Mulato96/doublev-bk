bcfe34e498783fa9656272a20b8c858e3075ff18
josemuesc <mulatoescobar48@gmail.com>
2025-03-09 17:40:37 -0500

    proyecto base

---
+++ src/main/java/com/doublev/farmatodo/DoublevApplication.java
@@ -0,0 +1,13 @@
+package com.doublev.farmatodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoublevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoublevApplication.class, args);
+	}
+
+}
